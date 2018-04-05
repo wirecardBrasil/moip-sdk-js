4c6e8ffafd4eb03fb58522d680ed75ba63bfb44f
Bruno Oliveira <bruno.oliveira@intelipost.com.br>
2018-04-05 20:00:25 -0300

    Add option for ionic 3

--- src/encrypt.js
+++ src/encrypt.js
@@ -6,36 +6,37 @@ export default class Encrypt {
 
 	static encrypt(value, pubKey) {
 		if (!this.encrypter && !this.encrypterName && typeof JSEncrypt !== 'undefined') {
-      this.encrypter = JSEncrypt;
-      this.encrypterName = 'js';
+      		this.encrypter = JSEncrypt;
+      		this.encrypterName = 'js';
 		}
 
 		if(this.encrypter && this.encrypterName) {
 			switch (this.encrypterName.toLowerCase()){
-        case 'js':
-          return this.jsEncrypt(value, pubKey);
-          break;
+        		case 'js':
+        		case 'ionic':
+      				return this.jsEncrypt(value, pubKey);
+          			break;
 				case 'node':
 					return this.nodeRSA(value, pubKey);
 					break;
-        case 'react-native':
-          return this.reactNativeRsa(value, pubKey);
-          break;
+        		case 'react-native':
+          			return this.reactNativeRsa(value, pubKey);
+          			break;
 			}
 		}
 	}
 
 	static jsEncrypt(value, pubKey) {
-    return new Promise((resolve) => {
-      const jsEncrypt = new this.encrypter({ default_key_size: 2048 });
-      jsEncrypt.setPublicKey(pubKey);
-      return resolve(jsEncrypt.encrypt(value));
-    });
+    	return new Promise((resolve) => {
+      		const jsEncrypt = new this.encrypter({ default_key_size: 2048 });
+      		jsEncrypt.setPublicKey(pubKey);
+      		return resolve(jsEncrypt.encrypt(value));
+    	});
 	}
 
 	static nodeRSA(value, pubKey) {
 		return new Promise((resolve) => {
-      const nodeRsa = new this.encrypter(pubKey);
+      		const nodeRsa = new this.encrypter(pubKey);
 			return resolve(nodeRsa.encrypt(value, 'base64'));
 		});
 	}
